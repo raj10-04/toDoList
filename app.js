afcf1f9096714c21e555391f42c96ddb9d0534c7
raj10-04 <raj100shah1@gmail.com>
2022-08-28 18:49:18 +0530

    css and page updated

--- app.js
+++ app.js
@@ -5,21 +5,24 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+var items = ['eat more'];
+
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 app.get("/",function(req,res){ 
-       
-    var today  = new Date();
+
+    var today = new Date();
     var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
     var day = today.toLocaleDateString('en-us',options);
 
 
-    res.render('list',{day:day});
+    res.render('list',{kindOfDay:day, newListItem:items});
 });
 
 app.post("/",function(req,res){
     var item = req.body.newItem;
-    console.log(item);
+    items.push(item);
+    res.redirect('/');
 });
 
 app.listen(4000,function(){
