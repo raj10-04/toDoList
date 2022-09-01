b814ac08c07ced20f29c856dfef8ff695bcddfd5
raj100shah1 <raj100shah1@gmail.com>
2022-09-01 23:34:04 +0530

    updated /work

--- app.js
+++ app.js
@@ -6,9 +6,12 @@ const bodyParser = require("body-parser");
 const app = express();
 
 var items = ['eat more'];
+var workItems = ['work'];
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
 app.get("/",function(req,res){ 
 
     var today = new Date();
@@ -16,14 +19,28 @@ app.get("/",function(req,res){
     var day = today.toLocaleDateString('en-us',options);
 
 
-    res.render('list',{kindOfDay:day, newListItem:items});
+    res.render('list',{listTitle:day, newListItem:items});
 });
 
 app.post("/",function(req,res){
-    var item = req.body.newItem;
-    items.push(item);
-    res.redirect('/');
+    let item = req.body.newItem;
+    if (req.body.list==="Work"){
+        workItems.push(item);
+        res.redirect('/work');
+    }else{
+        items.push(item);
+        res.redirect('/');
+    }
 });
+
+app.get("/work",function(req,res){
+    res.render('list',{listTitle:"Work Lists", newListItem:workItems});
+});
+// app.post('/work',function(req,res){
+//     let workItem = req.body.newItem;
+//     workItems.push(item);
+//     res.redirect('/work');
+// })
 
 app.listen(4000,function(){
     console.log("server is running");
