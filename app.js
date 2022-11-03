fc9cd238d11931082fcab3e57555a197c2cd3793
raj10-04 <raj100shah1@gmail.com>
2022-11-04 01:23:54 +0530

    starting db

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
 const date = require(__dirname+'/date.js');
 const app = express();
 
@@ -12,6 +13,37 @@ var workItems = ['work'];
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
+
+mongoose.connect('mongodb+srv://raj100shah1:rajshah00@newsdb.5czx041.mongodb.net/todolist');
+
+const itemsSchema = {
+    name:String
+};
+
+const Item = mongoose.model('Item',itemsSchema);
+
+const item1 = new Item({
+    name:"Welcome to your to do list!"
+});
+
+const item2 = new Item({
+    name:"Hit the + button to add an item"
+});
+
+const item3 = new Item({
+    name:"<--- Hit this to delete an item"
+});
+
+const defaultItems = [item1,item2,item3];
+
+Item.insertMany(defaultItems,function(err){
+    if (err){
+        console.log(err)
+    }else{
+        console.log("success db saved")
+    }
+
+})
 
 app.get("/",function(req,res){ 
 
